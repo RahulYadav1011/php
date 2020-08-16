cf2db89820a8be1dcb12010f2ad0171303dd6d1a
RahulYadav1011 <52218278+RahulYadav1011@users.noreply.github.com>
2020-08-16 15:03:05 +0530

    Add files via upload

---
+++ rahul.php
@@ -0,0 +1,14 @@
+﻿
+
+<?php
+// the message
+$msg = "First line of text\n Second line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("rahulyadavyadav306@gmail.com","About hava any query",$msg);
+echo "mail is sended" ;
+?> 
+  
